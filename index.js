e7a5a4e2c8f61656dd067ba8efa0a23164e001d8
parmeshwar <rathodparmeshwar4321@gmail.com>
2021-08-10 13:01:23 +0530

    main

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.get("/", (req, res) => {
-    res.sendFile("/index");
+    res.sendFile("/index.html");
 });
 
 app.post("/send_email", (req, res) => {
