11fb7816b6a2ab7405d4d8d003251dd44bc15a85
parmeshwar <rathodparmeshwar4321@gmail.com>
2021-08-10 11:02:52 +0530

    uploading...files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+app.use(express.static('public'))
+    // Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get("/", (req, res) => {
+    res.sendFile("/index");
+});
+
+app.post("/send_email", (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(PORT, () => { console.log(`YOUR SERVER IS RUNNING AT PORT:: ${PORT}`); })
